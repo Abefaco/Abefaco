0490b04ac935c9926dc93e9e436e0f06ee551003
Abefaco <abefaco@gmail.com>
2026-04-07 13:04:59 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
-// Aguarda a página carregar completamente
 window.onload = function() {
     var btn = document.getElementById('btnAcessar');
     
     if (btn) {
-        btn.addEventListener('click', function() {
+        btn.onclick = function() {
             var emailUser = document.getElementById('email').value.trim().toLowerCase();
             var codUser = document.getElementById('codigo').value.trim();
             var msg = document.getElementById('mensagem');
@@ -13,18 +12,19 @@ window.onload = function() {
                 return;
             }
 
-            msg.innerHTML = "<span style='color:blue;'>Verificando dados...</span>";
+            msg.innerHTML = "<span style='color:blue;'>Validando acesso...</span>";
 
             Papa.parse("database.csv", {
                 download: true,
                 header: true,
                 skipEmptyLines: true,
+                dynamicTyping: true, // Ajuda a ler números corretamente
                 complete: function(res) {
                     var lista = res.data;
                     var encontrado = null;
 
                     for (var i = 0; i < lista.length; i++) {
-                        // Compara o número do trabalho
+                        // Compara o código ignorando espaços
                         if (String(lista[i].Número).trim() === codUser) {
                             encontrado = lista[i];
                             break;
@@ -37,16 +37,16 @@ window.onload = function() {
                             localStorage.setItem('trabalhoAtivo', JSON.stringify(encontrado));
                             window.location.href = "dashboard.html";
                         } else {
-                            msg.innerHTML = "<span style='color:red;'>E-mail não autorizado para este código.</span>";
+                            msg.innerHTML = "<span style='color:red;'>E-mail não autorizado para este trabalho.</span>";
                         }
                     } else {
                         msg.innerHTML = "<span style='color:red;'>Código não encontrado.</span>";
                     }
                 },
                 error: function() {
-                    msg.innerHTML = "<span style='color:red;'>Erro ao carregar banco (database.csv).</span>";
+                    msg.innerHTML = "<span style='color:red;'>Erro ao carregar o arquivo database.csv.</span>";
                 }
             });
-        });
+        };
     }
 };
